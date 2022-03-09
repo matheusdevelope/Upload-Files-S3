6131be5dafde41de7e41fa22b8dbbe8f93ba4171
Matheus <emersonmatheustu@gmail.com>
2022-03-09 17:59:41 -0400

    variavel incoreta

--- src/config.js
+++ src/config.js
@@ -7,7 +7,7 @@ const name = {
 async function GetConfig() {
   try {
     const config = await keytar.getPassword(name.service, name.acount);
-    Config = JSON.parse(config);
+    //  Config = JSON.parse(config);
     if (config) return JSON.parse(config);
   } catch (e) {
     return Promise.reject(e);
@@ -18,9 +18,9 @@ async function SetConfig(config) {
   try {
     keytar.setPassword(name.service, name.acount, JSON.stringify(config));
     console.log(
-      "O serviço será fechado e deve ser reaberto para que as alterações tenham efeito.\n\n\n"
+      "O serviço precisa ser fechado e deve ser reaberto para que as alterações tenham efeito.\n\n\n"
     );
-    setTimeout(process.exit, 3000);
+    //  setTimeout(process.exit, 3000);
     return;
   } catch (err) {
     return Promise.reject("Erro ao salvar configurações:\n->", err);
