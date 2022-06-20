33ce429a0d20f7b01d3a475173689aa577d655f0
Matheus <emersonmatheustu@gmail.com>
2022-06-20 15:46:40 -0400

    ajust client path output directory

--- Client/vite.config.ts
+++ Client/vite.config.ts
@@ -1,13 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-
+import { resolve } from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
     port: 3001,
   },
   build: {
-    outDir: "../Server/dist/client",
+    outDir: resolve(__dirname, "..", "Server", "dist", "client"),
   },
   plugins: [react()],
 });
