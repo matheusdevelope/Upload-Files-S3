1e08d9fa98ea406b836db389024ef22cdfc28e58
Matheus <emersonmatheustu@gmail.com>
2022-06-22 11:37:10 -0400

    .

--- Server/dist/database/data-source.js
+++ Server/dist/database/data-source.js
@@ -6,7 +6,7 @@ require("reflect-metadata");
 var typeorm_1 = require("typeorm");
 exports.AppDataSource = new typeorm_1.DataSource({
     type: "sqlite",
-    database: (0, path_1.resolve)(__dirname, "database.sqlite"),
+    database: (0, path_1.resolve)(__dirname, "..", "..", "sqlite3", "database.sqlite"),
     synchronize: true,
     logging: false,
     entities: [__dirname + "/entity/**/*.{js,ts}"],
