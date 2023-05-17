b092d3d0f4ecfbacebae8e1cff3d1a3cf1af36fe
Matheus <emersonmatheustu@gmail.com>
2023-05-17 14:40:13 -0400

    add sudo

--- Client/src/services/Axios.ts
+++ Client/src/services/Axios.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const Axios = axios.create({
-  baseURL:  window.location.host,
+  baseURL:  window.location.hostname + ":" + window.location.port,
   headers: {},
 });
